c457102ba9e933483d7294c661c307c625cf1b10
chandini <chandnimothera890@gmail.com>
2022-04-17 16:29:36 +0530

    I was changed my output two times

--- abc.java
+++ abc.java
@@ -6,6 +6,8 @@ public class abc{
     // for loop  
     for (int i = 1; i <= n; ++i) {
       System.out.println("My name is Chandni");
+      System.out.println("My name is Bhushan");
+      System.out.println("My name is Sonu");
     }
   }
 }
